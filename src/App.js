2044d2130c103fd08c5ea0adc1a54e512696a59c
JayB1308 <jbudhadev@gmail.com>
2022-04-12 20:41:26 +0530

    Added better design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import UserList from './components/UserList';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className='container'>
+    <h1 className='display-1 text-center'>Welcome!</h1>
+    <div className="row">
+    <div className="col">
+    <PostForm /></div>
+    <div className="col">
+    <UserList />
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
